9409d394246eb59270ec4308b4e7d161a2a4fa27
saad mouden <moudensaad03@gmail.com>
2024-05-19 15:52:13 +0200

    fix bugs

--- src/Controller/HistoricController.php
+++ src/Controller/HistoricController.php
@@ -13,6 +13,13 @@ class HistoricController extends AbstractController
     public function index(Security $security): Response
     {
         $user = $security->getUser(); // Récupère l'utilisateur connecté
+        if (!$user) {
+            // Set a flash message
+            $this->addFlash('warning', 'You must be logged in.');
+
+            // Rediriger vers la page de connexion si l'utilisateur n'est pas connecté
+            return $this->redirectToRoute('app_login'); // Assurez-vous que 'login_route' est le nom correct de votre route de connexion
+        }
         $recipes = $user->getRecipe(); // Récupère les recettes de cet utilisateur
 
         return $this->render('historic/index.html.twig', [
